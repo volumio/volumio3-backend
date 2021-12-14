9fb80bafa91535b717f63dd38efcea7c666fa7b9
Volumio <info@volumio.org>
2021-12-14 17:17:02 +0000

    Proper fetching of albumart in state

--- app/plugins/system_controller/volumiodiscovery/index.js
+++ app/plugins/system_controller/volumiodiscovery/index.js
@@ -428,7 +428,6 @@ ControllerVolumioDiscovery.prototype.getThisDevice = function () {
   var self = this;
 
   self.logger.info('Discovery: Getting this device information');
-  var artURL = '';
   var thisDevice = {};
   var thisState = self.commandRouter.volumioGetState();
   var ipAddresses = self.commandRouter.executeOnPlugin('system_controller', 'network', 'getCachedPAddresses', '');
@@ -443,9 +442,9 @@ ControllerVolumioDiscovery.prototype.getThisDevice = function () {
   thisDevice.name = self.commandRouter.sharedVars.get('system.name');
   thisDevice.type = config.get('device_type', 'device');
   thisDevice.serviceName = config.get('service');
-  
+  var artURL = thisDevice.host + '/albumart';
   if(thisState.albumart !== undefined && thisState.albumart !== null) {
-	artURL = thisState.albumart.indexOf('http://') >= 0 ? thisState.albumart : thisDevice.host + thisState.albumart;
+	artURL = (thisState.albumart.startsWith('http://') || thisState.albumart.startsWith('https://')) ? thisState.albumart : thisDevice.host + thisState.albumart;
   }
   
   thisDevice.state = {
@@ -454,7 +453,7 @@ ControllerVolumioDiscovery.prototype.getThisDevice = function () {
     mute: thisState.mute,
     artist: thisState.artist,
     track: thisState.title,
-    albumart: artUrl
+    albumart: artURL
   };
 
   return thisDevice;
