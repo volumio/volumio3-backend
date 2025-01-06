ff57fc9077cd603356142a6d696bfd71d2d56aa8
bklebke <22566805+bklebke@users.noreply.github.com>
2025-01-06 10:33:24 -0800

    Update aistereo-unmute.sh
    better comments

--- app/plugins/system_controller/i2s_dacs/scripts/aistereo-unmute.sh
+++ app/plugins/system_controller/i2s_dacs/scripts/aistereo-unmute.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Unmute Master Playback ZC; card 2 on RPi
+# Unmute Master Playback ZC for Audio Injector Stereo; card 2 on RPi
 amixer -c 2 sset 'Master Playback ZC' unmute
 
-# Unmute Output Mixer HiFi; card 2 on RPi
+# Unmute Output Mixer HiFi Audio Injector Stereo; card 2 on RPi
 amixer -c 2 sset 'Output Mixer HiFi' unmute
