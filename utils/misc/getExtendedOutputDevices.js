c92fc088ac05dd96ab56e166e616e84679a7e0bd
Volumio <info@volumio.org>
2025-09-02 09:25:18 +0000

    Provide normal object

--- utils/misc/getExtendedOutputDevices.js
+++ utils/misc/getExtendedOutputDevices.js
@@ -2,9 +2,17 @@ var io = require('socket.io-client');
 
 var socket = io.connect('http://localhost:3000');
 
-socket.emit('getExtendedOutputDevices', '');
+socket.on('connect', function() {
+    socket.emit('getExtendedOutputDevices', '');
+    socket.emit('getOutputDevices', '');
+});
+
+socket.on('pushOutputDevices', function(data) {
+    console.log('Push Output Devices:');
+    console.log(JSON.stringify(data, null, 2));
+});
 
 socket.on('pushExtendedOutputDevices', function(data) {
+    console.log('Push Extended Output Devices:');
     console.log(JSON.stringify(data, null, 2));
-    process.exit(0);
 });
